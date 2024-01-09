34952df35860e9f1f0a85bf6a3193d35223aedd6
Ahmed Rangel <ahmedrangel@outlook.com>
2024-01-09 17:52:38 -0500

    chore: updated example

--- example/example.js
+++ example/example.js
@@ -2,12 +2,6 @@ import { randUA } from "@ahmedrangel/rand-user-agent";
 
 // ...
 
-// Fetching the raw JSON URL file from rand-user-agent repository
-const response = await fetch("https://raw.githubusercontent.com/WebScrapingAPI/rand-user-agent/master/data/user-agents.json");
-
-// Using Coudflare R2
-// const response = await env.YOUR_BUCKET.get("user-agents.json");
-const jsonData = await response.json();
-const agent = randUA(jsonData, "desktop");
+const agent = randUA("desktop");
 
 console.log(agent);
