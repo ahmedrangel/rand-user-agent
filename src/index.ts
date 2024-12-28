c9119ed3c1e57350a382ab1f342858d9f2c93827
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-28 01:02:54 -0500

    ci: use node v22

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { JSONfrequencyNormalize, JSONIsFrequency, JSONinterval, randomElement } from "./helpers.js";
-import data from "./data/user-agents.json" assert { type: "json" };
+import data from "./data/user-agents.json" with { type: "json" };
 export const randUA = (
   device: string,
   browser: string | null = null,
